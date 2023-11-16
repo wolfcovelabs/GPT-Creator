a3b106233fa664fca602d4aad15d01df0efb4b50
wolfcovelabs <150466261+wolfcovelabs@users.noreply.github.com>
2023-11-16 15:27:30 -0500

    Update RAG-assignment.py

--- RAG-assignment.py
+++ RAG-assignment.py
@@ -6,3 +6,15 @@ from streamlit.logger import get_logger
 
 st.title('Wolf Cove Labs')
 st.subheader('My Heading')
+
+
+file_path = "2023q3-alphabet-earnings-release.pdf"
+
+file = client.files.create(
+  file=open(file_path, "rb"),
+  purpose='assistants'
+)
+
+st.header("GPT 4 Retriever Assistants")
+user_message = st.text_input("Pls enter your question:")
+submit = st.button("submit", type="primary")
