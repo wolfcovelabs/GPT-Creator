abec1cfd880c9c56cc559285f690c1c35a8a7608
wolfcovelabs <150466261+wolfcovelabs@users.noreply.github.com>
2023-11-16 15:31:14 -0500

    Update RAG-assignment.py

--- RAG-assignment.py
+++ RAG-assignment.py
@@ -8,7 +8,7 @@ st.title('Wolf Cove Labs')
 st.subheader('My Heading')
 
 
-file_path = "https://abc.xyz/assets/4a/3e/3e08902c4a45b5cf530e267cf818/2023q3-alphabet-earnings-release.pdf"
+file_path = "2023q3-alphabet-earnings-release.pdf"
 
 file = client.files.create(
   file=open(file_path, "rb"),
